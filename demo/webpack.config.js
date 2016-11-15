da6b57715935316f959d526fd69a3b0036d28a53
edeustace <ed.eustace@gmail.com>
2016-11-15 16:27:13 +0000

    use explicit loader see: https://github.com/webpack/webpack/releases/tag/v2.1.0-beta.26

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,27 +1,27 @@
 const path = require('path');
 
 module.exports = {
-    entry: {
-        demo: './entry.js'
-    },
-    output: {
-        path: path.resolve(__dirname),
-        filename: 'bundle.js',
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.jsx$/,
-                loader: 'babel',
-                query: {
-                    presets: ['react']
-                }
-            },
-            {
-                test: /\.less$/,
-                loader: "style!css!less"
-            }
-        ]
-    }
+  entry: {
+    demo: './entry.js'
+  },
+  output: {
+    path: path.resolve(__dirname),
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react']
+        }
+      },
+      {
+        test: /\.less$/,
+        loader: "style!css!less"
+      }
+    ]
+  }
 };
 
